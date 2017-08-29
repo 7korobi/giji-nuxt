ac7e58aa5722de31d5e15ee4ed04e2e82f1fe61e
7korobi <7korobi@gmail.com>
2017-08-29 18:02:13 +0900

    hsla fix.

--- components/layout-footer.vue
+++ components/layout-footer.vue
@@ -2,12 +2,12 @@
 人狼議事キャラセット by りりんら
 管理 [sol・la]()[ななころび](mailto:7korobi@gmail.com)
 
-```
+``` copyright
 下記の場所以外では、人狼議事内キャラチップ
 の利用を許諾しておりません。ご了承ください。
 ```
 
-```
+``` copyright
 議事総合トップ
 人狼議事lobby
 人狼議事morphe
@@ -27,6 +27,8 @@ Sangria
 <style lang="stylus" scoped>
 div
   text-align: right
-code
+.lang-copyright
+  background-color: white
+  color: red
   text-align: left
 </style>
